7ffba3d601953a761de74120e6dc3269515f0cd1
DanielyanSmbat <smbat_danielyan@edu.aua.am>
2021-04-29 10:15:10 -0500

    Cpp2

--- fib.cpp
+++ fib.cpp
@@ -2,9 +2,32 @@
 
 
 
-int calc_fibo(int x) 
+
+void print_binary(int x)
+{	
+	int i,bin=0;
+	printf("The binary representation of %d is: ",x );
+	for(i=31;i>=0;i--)
+	{
+		bin = x>>i;
+		if(bin & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+	printf("\n");
+}
+
+
+
+int main()
 {
+	int x=0;
+
+	printf("Hello dear friend, Please insert your age: ");
+	scanf("%d",&x);
 	if(x == 0)
+	
 	{
 		printf("Insert a valid number \n");
 	}	
@@ -12,45 +35,19 @@ int calc_fibo(int x)
 	{
 		int temp,sum = 0;
 		int fib1,fib2 = 1;
-	
+
 		while(fib2 < x)
-	
+
 		{
 			temp = fib1;
 			fib1 = fib2;
-		      	fib2 += temp;
+			fib2 += temp;
 			sum += fib1;
 		}
-		printf("The sum of the Fibonacci numbers less than %d is : %d \n",x, sum);
-		
-	
+		printf("The sum of the Fibonacci numbers less than %d is : %d \n",x, sum);			
 	}
 
-	return 0;
-}
+print_binary(x);
 
-void print_binary(int x)
-{
-	int i=0;
-	printf("The binary representation of %d is: ",x );
-	while(x>0)
-	{
-		i = x%2;
-		x /= 2;
-		printf("%d",i);
-		
-	}
-
-}
-
-int main()
-{
-	int x=35;
-
-	//printf("Hello dear friend, Please insert your age: ");
-	//scanf("%d",&x);
-	calc_fibo(x);
-	print_binary(x);
-
-	return 0;
+return 0;
 }
